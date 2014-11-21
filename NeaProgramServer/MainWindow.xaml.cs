d81418163464466a586f265096659c15f4f17e1d
Armienn <nigathr@gmail.com>
2014-11-21 18:07:38 +0100

    Base functionality is now working locally

--- NeaProgramServer/MainWindow.xaml.cs
+++ NeaProgramServer/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
+using System.IO;
 
 namespace NeaProgramServer {
 	/// <summary>
@@ -24,32 +25,53 @@ namespace NeaProgramServer {
 			css = new Process();
 			css.StartInfo.FileName = "C:\\css\\srcds.exe";
 			css.StartInfo.Arguments = "-console -game cstrike -insecure +maxplayers 22 +map de_dust";
-			minecraft = new Process();//Java -Xmx1024M -Xms1024M -jar minecraft_server.1.8.1-pre3.jar
-			minecraft.StartInfo.FileName = "Java";
-			minecraft.StartInfo.Arguments = "-Xmx1024M -Xms1024M -jar \"C:\\Users\\Kristjan\\Dropbox\\Spil\\Minecraft server (2)\\minecraft_server.1.8.1-pre3.jar\"";
+			minecraft = new Process();
+			minecraft.StartInfo.WorkingDirectory = "C:\\Users\\Kristjan\\Dropbox\\Spil\\Minecraft server (2)";
+			minecraft.StartInfo.FileName = "serverjar.bat";
 		}
 
 		Process css;
 		Process minecraft;
 		Process terraria;
 
-		private void Button_Click(object sender, RoutedEventArgs e) {
-			process = new Process();
-			process.StartInfo.FileName = "C:\\css\\srcds.exe";
-			process.StartInfo.Arguments = "-console -game cstrike -insecure +maxplayers 22 +map de_dust";
-			process.Start();
-		}
-
-		private void Button_Click_1(object sender, RoutedEventArgs e) {
-			process.CloseMainWindow();
-		}
-
 		private void buttoncss_Click(object sender, RoutedEventArgs e) {
-
+			bool running = false;
+			try {
+				Process.GetProcessById(css.Id);
+				running = true;
+			}
+			catch { }
+			if (running) {
+				css.CloseMainWindow();
+				css.WaitForExit();
+				statuscss.Content = "CURRENTLY STOPPED";
+				buttoncss.Content = "Start";
+			}
+			else {
+				css.Start();
+				statuscss.Content = "CURRENTLY RUNNING";
+				buttoncss.Content = "Stop";
+			}
 		}
 
 		private void buttonminecraft_Click(object sender, RoutedEventArgs e) {
-
+			bool running = false;
+			try {
+				Process.GetProcessById(minecraft.Id);
+				running = true;
+			}
+			catch { }
+			if (running) {
+				minecraft.CloseMainWindow();
+				minecraft.WaitForExit();
+				statusminecraft.Content = "CURRENTLY STOPPED";
+				buttonminecraft.Content = "Start";
+			}
+			else {
+				minecraft.Start();
+				statusminecraft.Content = "CURRENTLY RUNNING";
+				buttonminecraft.Content = "Stop";
+			}
 		}
 
 		private void buttonterraria_Click(object sender, RoutedEventArgs e) {
