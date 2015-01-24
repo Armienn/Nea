d5c78e49caa06bbdb4e0dfeb8554f40699b42342
Armienn <nigathr@gmail.com>
2015-01-24 23:57:44 +0100

    Added initial Language project work

---
+++ Language/Sound.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Language {
+	struct Vowel : Sound {
+		
+	}
+
+	struct Sound {
+		Airstream airstream;
+		Initiation initiation;
+
+	}
+
+	enum Airstream { Egressive, Ingressive }
+
+	enum Initiation { Pulmonic, Glottalic, Lingual }
+
+	enum LabialPoint { None, Labial, Dental }
+
+	enum CoronalPoint { None, Labial, Dental, Alveolar, PostAlveolar, Palatal }
+
+	enum DorsalPoint { None, PostAlveolar, Palatal, Velar, Uvular }
+
+	enum RadicalPoint { None, Pharyngeal }
+
+	enum LaryngealPoint { None, Pharyngeal, Epiglottal, Glottal }
+
+	//enum Mechanism { Pulmonic, Click, Ejective, Implosive }
+
+	//enum PulmonicType { Consonant, Vowel }
+
+	//enum ConsonantArticulation { Labial }
+}
