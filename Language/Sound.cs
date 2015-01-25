e5bb09fb8f7f48e082561a9547ecbc2e338d47d1
Armienn <nigathr@gmail.com>
2015-01-25 11:24:21 +0100

    Further work on sound system

--- Language/Sound.cs
+++ Language/Sound.cs
@@ -5,8 +5,34 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Language {
-	struct Vowel : Sound {
-		
+	struct Articulation {
+		public ObstructionPoint point;
+		public Manner manner;
+		protected static const ObstructionPoint[] allowedpoints = new ObstructionPoint[] { 
+			ObstructionPoint.None, 
+			ObstructionPoint.Labial, 
+			ObstructionPoint.Dental, 
+			ObstructionPoint.Alveolar, 
+			ObstructionPoint.PostAlveolar, 
+			ObstructionPoint.Palatal, 
+			ObstructionPoint.Velar, 
+			ObstructionPoint.Uvular, 
+			ObstructionPoint.Pharyngeal, 
+			ObstructionPoint.Epiglottal, 
+			ObstructionPoint.Glottal };
+
+		public static ObstructionPoint[] AllowedPoints { get { return allowedpoints; } }
+	}
+
+	struct LabialArticulation : Articulation {
+		protected static const ObstructionPoint[] allowedpoints = new ObstructionPoint[] { 
+			ObstructionPoint.None, 
+			ObstructionPoint.Labial, 
+			ObstructionPoint.Dental, 
+			ObstructionPoint.Alveolar, 
+			ObstructionPoint.PostAlveolar, 
+			ObstructionPoint.Palatal, 
+			ObstructionPoint.Velar };
 	}
 
 	struct Sound {
@@ -18,6 +44,10 @@ namespace Language {
 	enum Airstream { Egressive, Ingressive }
 
 	enum Initiation { Pulmonic, Glottalic, Lingual }
+
+	enum ObstructionPoint { None, Labial, Dental, Alveolar, PostAlveolar, Palatal, Velar, Uvular, Pharyngeal, Epiglottal, Glottal }
+
+	enum Manner { Stop, Tap, Trill, Fricative, Approximant, Close, NearClose, CloseMid, Mid, OpenMid, NearOpen, Open }
 
 	enum LabialPoint { None, Labial, Dental }
 
